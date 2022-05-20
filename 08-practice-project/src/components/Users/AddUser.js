debd924858bc1c9083f2b246acf4386fe92f3bc4
jeraldpraveen <jerald.vit@gmail.com>
2022-05-20 14:55:56 +0530

    5.Managing AddUserState

--- 08-practice-project/src/components/Users/AddUser.js
+++ 08-practice-project/src/components/Users/AddUser.js
@@ -1,21 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "../UI/Card";
 import classes from "./AddUser.module.css";
 import Button from "../UI/Button";
 
 const AddUser = (props) => {
+  const [enteredUsername, setEnteredUsername] = useState("");
+  const [enteredAge, setEnteredAge] = useState("");
+
   const addUserHandler = (event) => {
     event.preventDefault();
-    alert("Success");
+    alert(enteredUsername + enteredAge);
+  };
+
+  const usernameChangeHandler = (event) => {
+    setEnteredUsername(event.target.value);
+  };
+
+  const ageChangeHandler = (event) => {
+    setEnteredAge(event.target.value);
   };
 
   return (
     <Card cssClass={classes.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">Username</label>
-        <input id="username" type="text" />
+        <input id="username" type="text" onChange={usernameChangeHandler} />
         <label htmlFor="age">Age</label>
-        <input id="age" type="number" />
+        <input id="age" type="number" onChange={ageChangeHandler} />
         <Button type="submmit">Add User</Button>
       </form>
     </Card>
