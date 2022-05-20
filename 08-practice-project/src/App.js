dd7ccff4889458e4d44b4228e8e950ff90337065
jeraldpraveen <jerald.vit@gmail.com>
2022-05-20 12:17:41 +0530

    3.AddUser Component

--- 08-practice-project/src/App.js
+++ 08-practice-project/src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import AddUser from "./components/Users/AddUser";
 
 function App() {
   return (
     <div className="App">
       <h1>Hi</h1>
+      <AddUser />
     </div>
   );
 }
