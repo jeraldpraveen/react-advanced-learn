da254e892bb0bea8853ea12002365c1e522c8a66
jeraldpraveen <jerald.vit@gmail.com>
2022-05-22 03:36:32 +0530

    33.Component Updates In Action

--- 12-usememo-usecallback/src/App.js
+++ 12-usememo-usecallback/src/App.js
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./App.css";
+import Button from "./components/UI/Button/Button";
 
 function App() {
+  const [showPara, setShowPara] = useState(false);
+
+  console.log("APP RUNNING");
+
+  const togleParaHandler = () => {
+    if (!showPara) setShowPara(true);
+    else setShowPara(false);
+  };
+
   return (
     <div className="app">
       <h1>Hi there!</h1>
+      {showPara && <p>This is New</p>}
+      <Button onClick={togleParaHandler}>Show Para !</Button>
     </div>
   );
 }
