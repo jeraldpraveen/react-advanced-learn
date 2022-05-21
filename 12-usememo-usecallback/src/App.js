317d4a063101b557273b718e56653c0dc98fe776
jeraldpraveen <jerald.vit@gmail.com>
2022-05-22 05:00:04 +0530

    39.REACT HOOK===> useMemo

--- 12-usememo-usecallback/src/App.js
+++ 12-usememo-usecallback/src/App.js
@@ -1,12 +1,14 @@
-import React, { useState, useCallback } from "react";
+import React, { useState, useCallback, useMemo } from "react";
 
 import "./App.css";
 import Button from "./components/UI/Button/Button";
 import Demo from "./components/Demo/Demo";
+import DemoList from "./components/Demo/DemoList";
 
 function App() {
   const [showPara, setShowPara] = useState(false);
   const [allowToggle, setAllowToggle] = useState(false);
+  const [listTitle, setListTitle] = useState("My List");
 
   console.log("APP RUNNING");
 
@@ -24,12 +26,20 @@ function App() {
     setAllowToggle(true);
   };
 
+  const changeTitleHandler = () => {
+    setListTitle("New Title");
+  };
+
+  const listItems = useMemo(() => [5, 3, 1, 10, 9], []);
+
   return (
     <div className="app">
       <h1>Hi there!</h1>
       <Demo show={showPara} />
       <Button onClick={allowToggleHandler}>Allow Toggle</Button>
       <Button onClick={togleParaHandler}>Show Para !</Button>
+      <DemoList title={listTitle} items={listItems} />
+      <Button onClick={changeTitleHandler}>Change List Title</Button>
     </div>
   );
 }
