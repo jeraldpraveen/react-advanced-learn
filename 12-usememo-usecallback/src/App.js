addde147b5593b5a5184b48893e303fbe84e1674
jeraldpraveen <jerald.vit@gmail.com>
2022-05-22 04:23:42 +0530

    38.usecallback + Dependencies

--- 12-usememo-usecallback/src/App.js
+++ 12-usememo-usecallback/src/App.js
@@ -6,18 +6,29 @@ import Demo from "./components/Demo/Demo";
 
 function App() {
   const [showPara, setShowPara] = useState(false);
+  const [allowToggle, setAllowToggle] = useState(false);
 
   console.log("APP RUNNING");
 
   const togleParaHandler = useCallback(() => {
-    if (!showPara) setShowPara(true);
-    else setShowPara(false);
-  }, []);
+    if (allowToggle) {
+      if (!showPara) {
+        setShowPara(true);
+      } else {
+        setShowPara(false);
+      }
+    }
+  }, [allowToggle, showPara]);
+
+  const allowToggleHandler = () => {
+    setAllowToggle(true);
+  };
 
   return (
     <div className="app">
       <h1>Hi there!</h1>
-      <Demo show={false} />
+      <Demo show={showPara} />
+      <Button onClick={allowToggleHandler}>Allow Toggle</Button>
       <Button onClick={togleParaHandler}>Show Para !</Button>
     </div>
   );
