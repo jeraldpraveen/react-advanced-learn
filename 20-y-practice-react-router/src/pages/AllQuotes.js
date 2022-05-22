9c4f83f001bf83c1051ca3575d2e303df6e84d04
jeraldpraveen <jerald.vit@gmail.com>
2022-05-22 22:18:09 +0530

    55.Loading DUMMY_QUOTES & Add A Quote

--- 20-y-practice-react-router/src/pages/AllQuotes.js
+++ 20-y-practice-react-router/src/pages/AllQuotes.js
@@ -1,7 +1,21 @@
 import React from "react";
+import QuoteList from "../components/quotes/QuoteList";
+
+const DUMMY_QUOTES = [
+  {
+    id: "q1",
+    author: "Jerald",
+    text: "Learning React Is Fun",
+  },
+  {
+    id: "q2",
+    author: "Praveen",
+    text: "React Is Great",
+  },
+];
 
 const AllQuotes = () => {
-  return <div>AllQuotes</div>;
+  return <QuoteList quotes={DUMMY_QUOTES} />;
 };
 
 export default AllQuotes;
