532f097143b44299db7c844c5fed822dce00661e
jeraldpraveen <jerald.vit@gmail.com>
2022-05-22 20:22:38 +0530

    47.Need For "switch"

--- 20-spa-react-router/src/pages/Products.js
+++ 20-spa-react-router/src/pages/Products.js
@@ -1,13 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Products = () => {
   return (
     <section>
       <h1>Products</h1>;
       <ul>
-        <li>Product 1</li>
-        <li>Product 2</li>
-        <li>Product 3</li>
+        <li>
+          <Link to="/products/1">Product 1</Link>
+        </li>
+        <li>
+          <Link to="/products/2">Product 2</Link>
+        </li>
+        <li>
+          <Link to="/products/3">Product 3</Link>
+        </li>
       </ul>
     </section>
   );
