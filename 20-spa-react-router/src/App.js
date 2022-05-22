a38fb888b5a248f5c2e9a97b14e2708053e14b43
jeraldpraveen <jerald.vit@gmail.com>
2022-05-22 19:57:43 +0530

    42.Router ==> <Link> Tag

--- 20-spa-react-router/src/App.js
+++ 20-spa-react-router/src/App.js
@@ -1,19 +1,23 @@
 import { BrowserRouter, Route } from "react-router-dom";
 import Welcome from "./pages/Welcome";
 import Products from "./pages/Products";
+import MainHeader from "./components/MainHeader";
 
 function App() {
   return (
     <BrowserRouter>
-      <Route exact path="/">
-        <h1>Home</h1>
-      </Route>
-      <Route path="/welcome">
-        <Welcome />
-      </Route>
-      <Route path="/products">
-        <Products />
-      </Route>
+      <MainHeader />
+      <main>
+        <Route exact path="/">
+          <h1>Home</h1>
+        </Route>
+        <Route path="/welcome">
+          <Welcome />
+        </Route>
+        <Route path="/products">
+          <Products />
+        </Route>
+      </main>
     </BrowserRouter>
   );
 }
