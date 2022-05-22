040d134f661d7f31fbcc0079c3e2b246feaf328c
jeraldpraveen <jerald.vit@gmail.com>
2022-05-22 20:17:24 +0530

    45.Extracting Params From URL

--- 20-spa-react-router/src/pages/ProductDetails.js
+++ 20-spa-react-router/src/pages/ProductDetails.js
@@ -1,9 +1,13 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 
 const ProductDetails = () => {
+  const params = useParams();
+
   return (
     <section>
       <h1>ProductDetail</h1>
+      {params.productId}
     </section>
   );
 };
