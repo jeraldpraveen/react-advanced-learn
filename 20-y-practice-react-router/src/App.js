7c4297a0047baf0430c58162974c43d86c7e4382
jeraldpraveen <jerald.vit@gmail.com>
2022-05-22 20:58:15 +0530

    52.Router Practice Concepts

--- 20-y-practice-react-router/src/App.js
+++ 20-y-practice-react-router/src/App.js
@@ -1,5 +1,24 @@
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import AllQuotes from "./pages/AllQuotes";
+import NewQuote from "./pages/NewQuote";
+import QuoteDetail from "./pages/QuoteDetail";
+
 function App() {
-  return <div></div>;
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/quotes" exact>
+          <AllQuotes />
+        </Route>
+        <Route path="/quotes/:quoteId">
+          <QuoteDetail />
+        </Route>
+        <Route path="/new-quote">
+          <NewQuote />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
 export default App;
