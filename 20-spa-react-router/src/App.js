71e630df4d72beb4000ee96f5685bf1ab9279fd4
jeraldpraveen <jerald.vit@gmail.com>
2022-05-22 20:19:50 +0530

    46.Need For "exact"

--- 20-spa-react-router/src/App.js
+++ 20-spa-react-router/src/App.js
@@ -18,7 +18,7 @@ function App() {
         <Route path="/products">
           <Products />
         </Route>
-        <Route path="/product-details/:productId">
+        <Route path="/products/:productId">
           <ProductDetails />
         </Route>
       </main>
