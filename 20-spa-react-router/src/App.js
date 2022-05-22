15f205e897fe4cd8f76d7f1b442516932183accb
jeraldpraveen <jerald.vit@gmail.com>
2022-05-22 19:50:11 +0530

    41.Defining & Using Routes

--- 20-spa-react-router/src/App.js
+++ 20-spa-react-router/src/App.js
@@ -1,8 +1,20 @@
+import { BrowserRouter, Route } from "react-router-dom";
+import Welcome from "./pages/Welcome";
+import Products from "./pages/Products";
+
 function App() {
   return (
-    <div>
-      <h2>Let's get started!</h2>
-    </div>
+    <BrowserRouter>
+      <Route exact path="/">
+        <h1>Home</h1>
+      </Route>
+      <Route path="/welcome">
+        <Welcome />
+      </Route>
+      <Route path="/products">
+        <Products />
+      </Route>
+    </BrowserRouter>
   );
 }
 
