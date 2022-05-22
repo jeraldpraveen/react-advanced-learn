35981f57027a98b1f68d1beeb1547cc047d0bd6a
jeraldpraveen <jerald.vit@gmail.com>
2022-05-22 22:50:52 +0530

    57.ROUTER ==> useHistory

--- 20-y-practice-react-router/src/pages/NewQuote.js
+++ 20-y-practice-react-router/src/pages/NewQuote.js
@@ -1,9 +1,15 @@
 import React from "react";
 import QuoteForm from "../components/quotes/QuoteForm";
+import { useHistory } from "react-router-dom";
 
 const NewQuote = () => {
+  const history = useHistory();
   const addQuoteHandler = (quoteData) => {
     console.log(quoteData);
+    // Browser Back will Work
+    history.push("/quotes");
+    // Browser Back will NOT Work
+    // history.replace("/quotes");
   };
 
   return <QuoteForm onAddQuote={addQuoteHandler} />;
